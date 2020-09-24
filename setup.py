dae885ec1dbaea2c7103c70de2546331f02c7cb0
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-09-24 13:58:23 +0300

    Added MANIFEST.in file.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(["DataStructure/*.pyx", "DataStructure/Cache/*.pyx", "DataStructure/Cache/*.pxd"], compiler_directives={'language_level' : "3"}),
     name='NlpToolkit-DataStructure-Cy',
-    version='1.0.0',
+    version='1.0.1',
     packages=['DataStructure', 'DataStructure.Cache'],
     url='https://github.com/olcaytaner/DataStructure-Cy',
     license='',
