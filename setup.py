5939aa0bd4a6703ef92bbb3304ed70b667053091
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-09-26 11:26:44 +0300

    Added package_data option.

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from Cython.Build import cythonize
 setup(
     ext_modules=cythonize(["DataStructure/*.pyx", "DataStructure/Cache/*.pyx", "DataStructure/Cache/*.pxd"], compiler_directives={'language_level' : "3"}),
     name='NlpToolkit-DataStructure-Cy',
-    version='1.0.1',
+    version='1.0.2',
     packages=['DataStructure', 'DataStructure.Cache'],
+    package_data={'DataStructure': ['*.pxd', '*.pyx', '*.c'], 'DataStructure.Cache': ['*.pxd', '*.pyx', '*.c']},
     url='https://github.com/olcaytaner/DataStructure-Cy',
     license='',
     author='olcaytaner',
